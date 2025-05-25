ac3c2d0977fbb9710305a74672e636e6588d399a
Akshay <thakuraks019@gmail.com>
2025-05-25 17:57:58 +0530

    final commit

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -8,9 +8,9 @@ export const initFirebaseAdmin = () => {
   if (!apps.length) {
     initializeApp({
       credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
+        projectId: process.env.FIREBASE_PROJECT_ID!,
         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
       }),
     });
   }
